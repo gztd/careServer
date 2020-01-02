04917d0dc8df3847665da192ff8afd2d7e9891fd
randomzou <randomzou@gmail.com>
2020-01-02 18:11:24 +0800

    maybe useful for report

---
+++ src/main/java/org/r/system/cs/CsReportApplication.java
@@ -0,0 +1,12 @@
+package org.r.system.cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsReportApplication.class, args);
+	}
+}
